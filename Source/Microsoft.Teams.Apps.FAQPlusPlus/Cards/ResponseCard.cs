3235e9f6bf3d8c5fb0fcc6efa351558b9b133244
dabianchiGitHub <42076288+dabianchiGitHub@users.noreply.github.com>
2020-03-12 17:31:13 -0300

    cambios MS

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/ResponseCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/ResponseCard.cs
@@ -72,6 +72,11 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
                             UserQuestion = userQuestion,
                             KnowledgeBaseAnswer = answer,
                         }
+                    },
+                    new AdaptiveOpenUrlAction
+                    {
+                           Title = "Videoteca",
+                           Url = new Uri("https://web.microsoftstream.com/channel/6a92c909-c01f-4b81-90b5-70110a88e0d3/"),
                     }
                 }
             };
