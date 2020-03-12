af2d93db1c1c89d1a3d0093e2d26105c7132c4d8
dabianchiGitHub <42076288+dabianchiGitHub@users.noreply.github.com>
2020-03-12 17:25:36 -0300

    RollBack

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/UnrecognizedInputCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/UnrecognizedInputCard.cs
@@ -47,12 +47,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
                                 Text = FaqPlusPlusBot.AskAnExpert
                             },
                             UserQuestion = userQuestion
-                        }
-                    },
-                    new AdaptiveOpenUrlAction
-                    {
-                           Title = "Videoteca",
-                           Url = new Uri("https://web.microsoftstream.com/channel/6a92c909-c01f-4b81-90b5-70110a88e0d3"),
+                        },
                     }
                 }
             };
