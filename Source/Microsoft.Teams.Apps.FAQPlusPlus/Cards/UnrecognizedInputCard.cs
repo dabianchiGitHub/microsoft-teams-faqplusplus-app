84e9d715ec80433b8c4f13fd6787cf87c17b5344
dabianchiGitHub <42076288+dabianchiGitHub@users.noreply.github.com>
2020-03-12 17:13:34 -0300

    cambio coma
    coma del 50 al 51

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/UnrecognizedInputCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/UnrecognizedInputCard.cs
@@ -47,12 +47,12 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
                                 Text = FaqPlusPlusBot.AskAnExpert
                             },
                             UserQuestion = userQuestion
-                        },
-                    }
+                        }
+                    },
                     new AdaptiveOpenUrlAction
                     {
                            Title = "Videoteca",
-                           Url = new Uri("https://www.microsoft.com/"),
+                           Url = new Uri("https://web.microsoftstream.com/channel/6a92c909-c01f-4b81-90b5-70110a88e0d3"),
                     }
                 }
             };
